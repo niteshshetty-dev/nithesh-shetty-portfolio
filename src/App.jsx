fa6e23f0fd0c0f38b1cd9653acbecdc37e2288dc
nthshtty <nthsh6shtty@gmail.com>
2025-06-05 18:48:10 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./Components/About";
 import Skills from "./Components/Skills";
 import Projects from "./Components/Project";
 import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
